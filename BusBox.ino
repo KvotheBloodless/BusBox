8b315c7851823841a236b55dd0d64cb1e85fca87
Paul Willems <paulw@familyzone.com>
2019-07-17 07:06:00 +0100

    Added licence

--- BusBox.ino
+++ BusBox.ino
@@ -4,51 +4,50 @@
 //=====[ XBee ]=====
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
-// create reusable response objects for responses we expect to handle 
+
 Rx16Response rx16 = Rx16Response();
 Rx64Response rx64 = Rx64Response();
 
 //=====[ CONSTANTS ]=====
-const int bSize = 64;
-const char comfortLight = 'a'; 
-const char engineeringLight = 'b';
-const char hydraulic = 'c';
-const char outlet = 'd';
-const char fan = 'e';
-const char ventilation = 'f';
-const char freshWaterPump = 'g';
-const char wasteWaterPump = 'h';
-const char washDownPump = 'i';
-const char internet = 'j';
-const char entertainment = 'k';
-const char refrigeration = 'l';
-const char anchorLight = 'm';
-const char steamingLight = 'n';
-const char redOverGreenLight = 'o';
-const char deckLight = 'p';
-const char navigationLight = 'q';
-const char autopilot = 'r';
-const char instrument = 's';
-const char radio = 't';
-const char unused = 'z';
+static const int bSize = 64;
+static const char comfortLight = 'a'; 
+static const char engineeringLight = 'b';
+static const char hydraulic = 'c';
+static const char outlet = 'd';
+static const char fan = 'e';
+static const char ventilation = 'f';
+static const char freshWaterPump = 'g';
+static const char wasteWaterPump = 'h';
+static const char washDownPump = 'i';
+static const char internet = 'j';
+static const char entertainment = 'k';
+static const char refrigeration = 'l';
+static const char anchorLight = 'm';
+static const char steamingLight = 'n';
+static const char redOverGreenLight = 'o';
+static const char deckLight = 'p';
+static const char navigationLight = 'q';
+static const char autopilot = 'r';
+static const char instrument = 's';
+static const char radio = 't';
+static const char unused = 'z';
 
 //=====[ PIN CONSTANTS ]=====
 // Double check that circuitPins and circuitTypes are same length
 
 // const char circuitTypes[] = { comfortLight, engineeringLight, outlet, fan, ventilation, entertainment, refrigeration, redOverGreenLight, navigationLight, instrument, deckLight, unused }; // aft
 // const char circuitTypes[] = { ... }; // mid ships
-const char circuitTypes[] = { comfortLight, engineeringLight, outlet, fan, ventilation, entertainment, hydraulic, internet, anchorLight, steamingLight, deckLight, radio }; // forward
+static const char circuitTypes[] = { comfortLight, engineeringLight, outlet, fan, ventilation, entertainment, hydraulic, internet, anchorLight, steamingLight, deckLight, radio }; // forward
 
 // const int circuitPins[] = { 21, 22, 23, 13, 12, 11, 20, 19, 18, 10, 9, 8 }; // aft
 // const int circuitPins[] =   { ... }; // mid ships
-const int circuitPins[] = { 8, 9, 10, 11, 12, 13, 18, 19, 20, 21, 22, 23 }; // forward
+static const int circuitPins[] = { 8, 9, 10, 11, 12, 13, 18, 19, 20, 21, 22, 23 }; // forward
 
 const int numberOfCircuits = 12;
 
 //=====[ VARIABLES ]=====
 
 char command[25];
-bool validCommand = false;
 uint8_t option = 0;
 
 //=====[ SETUP ]=====
@@ -65,22 +64,20 @@ void setup() {
 
   Serial1.begin(9600);
   xbee.setSerial(Serial1);
+);
 }
 
 //=====[ PROGRAM ]=====
 
 void loop() {
 
-  while (fetchCommand() == true)
-    if (validCommand == true)
-      // parse command and act on it
-      processCommand();
+  if (fetchCommand() == true)
+    // parse command and act on it
+    processCommand();
 }
 
 //=====[ SUBROUTINES ]=====
 bool fetchCommand(void) {
-
-  validCommand = false;
 
   xbee.readPacket();
     
@@ -105,9 +102,7 @@ bool fetchCommand(void) {
         Serial.print("Invalid command: ");
         Serial.println(command);
       } else
-        validCommand = true;
-
-      return true;
+        return true;
     } else {
 
       Serial.print("Not what we were expecting. ApiId: ");  
